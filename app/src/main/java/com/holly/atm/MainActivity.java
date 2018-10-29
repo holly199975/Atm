c51dcbe3b67c41d2e5df47254ba1059caa2c3051
Holly Deng <holly199975@gmail.com>
2018-10-29 21:19:59 +0800

    登入修正

--- app/src/main/java/com/holly/atm/MainActivity.java
+++ app/src/main/java/com/holly/atm/MainActivity.java
@@ -1,18 +1,36 @@
 package com.holly.atm;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int RC_LOGIN=1;
     boolean logon = false;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_LOGIN){
+            if (resultCode == RESULT_OK){
+                String uid = data.getStringExtra("LOGIN_USERID");
+                String pw = data.getStringExtra("LOGIN_PASSWD");
+                Log.d("RESULT",uid + "/" + pw);
+            }else{
+                finish();
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (!logon){ //如沒登入開啟LoginActivity
             Intent intent = new Intent(this,LoginActivity.class);
-            startActivity(intent);
+            startActivityForResult(intent,RC_LOGIN);
         }
     }
 }
